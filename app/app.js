dbc614f7c7123cbf9a793aeeda01a0e164097344
suzana-roe <svzana07@gmail.com>
2021-08-10 23:58:49 +0100

    endpoints created

---
+++ app/app.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// The application layer uses registration classes
+const registration = require("../tissuebank.js");
+
+// The application layer talks to the data layer
+const data = require("../data/data.js");
+
+// Import express library.
+const express = require("express");
+
+// Create express application
+var app = express();
+
+// Add static files location
+app.use(express.static("static"));
+
+// Add /registration endpoint
+app.get("/registration/:code", function(req, res) {
+  // Return "Registration <code>"
+  res.send("Registration" + req.params.code);
+});
+
+// Add /registrations endpoint
+app.get("/registrations", function(req, res) {
+  // Return "All registrations"
+  res.send("All registrations");
+});
+
+// Start listening on port 3000
+app.listen(3000, function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Server started.");
+  });
--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("Hello, world!");
+"use strict";
+
+// Include the app.js file.
+// This will run the code.
+const app = require("./app/app");
